71721f8579e36d4ae0947382a730678f7e450dde
Rolice <l.gardev@intellisys.org>
2015-11-12 18:39:36 +0200

    Generic interface update.

--- src/Econt.php
+++ src/Econt.php
@@ -2,6 +2,7 @@
 namespace Rolice\Econt;
 
 use SimpleXMLElement;
+use Nathanmac\Utilities\Parser\Parser;
 
 use App;
 use Config;
@@ -29,6 +30,9 @@ class Econt
      */
     protected $password;
 
+    /**
+     * The constructor for app::singleton instance. Internally set up username and password for use from config.
+     */
     public function __construct()
     {
         $this->username = $this->username ?: Config::get('econt.username');
@@ -50,15 +54,21 @@ class Econt
 
     /**
      * Builds an Econt-compatible XML request
-     * @param string $type A type of the Econt request. Use RequestType constants if possible.
-     * @param array $data A user-defined, custom request structure of the XML file.
-     * @param string $root The name of the root XML element.
+     * @param SimpleXMLElement $xml Currently scoped XML representation object.
+     * @param array $data A user-defined, custom request structure for the XML file.
      * @return string Serialized result in XML format.
-     * @throws EcontException
      */
-    protected static function build(array $data)
+    protected function build(SimpleXMLElement $xml, array $data)
     {
-        //SimpleXMLElement::
+        foreach($data as $key => $value) {
+            if(!is_scalar($value)) {
+                $nested = $xml->addChild($key);
+                $this->build($nested, $value);
+                continue;
+            }
+
+            $xml->addChild($key, $value);
+        }
     }
 
     /**
@@ -67,29 +77,13 @@ class Econt
      * @return string Unserialized XML data in PHP
      * @throws EcontException
      */
-    protected static function parse($response)
+    protected function parse($response)
     {
-        $parser = new XML_Unserializer();
-        $status = $parser->unserialize($response);
-
-        if (PEAR::isError($status)) {
-            throw new EcontException('Failed serialize registration request.');
-        }
-
-        $result = $parser->getUnserializedData();
-
-        if (isset($result['error'])) {
-            $message = isset($result['error']['message']) ? $result['error']['message'] : '';
-            $code = isset($result['error']['code']) ? $result['error']['code'] : '';
-
-            throw new EcontException($message, $code);
-        }
-
-        return $result;
+        return Parser::xml($response);
     }
 
     /**
-     * Makes request to Econt servers
+     * Makes request to Econt servers (calls an end-point)
      * @param string $endpoint The end-point URL of the Econt server. Use Endpoint constants if applicable.
      * @param string $request The serialized XML content of the request.
      * @return string Raw response of the Econt servers to the given request.
@@ -108,14 +102,6 @@ class Econt
         return $response;
     }
 
-    public final static function test()
-    {
-        $result = self::build(RequestType::PROFILE, []);
-        $result = self::parse(self::request(Endpoint::SERVICE, $result, 'service_types'));
-
-        return $result;
-    }
-
     public final function order(Sender $sender, Receiver $receiver, Shipment $shipment)
     {
 
@@ -131,9 +117,9 @@ class Econt
             'request_type' => $type,
         ]);
 
-        $xml = new SimpleXMLElement('request');
-        array_walk_recursive($request, [$xml, 'addchild']);
+        $xml = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8"?><request/>');
+        $this->build($xml, $request);
 
-        die($xml->asXML());
+        return $this->call(Endpoint::SERVICE, $xml->asXML());
     }
 }
