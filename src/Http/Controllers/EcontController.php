70466ef8c4cd4bf2f3129eee791addef8259b88a
Rolice <l.gardev@intellisys.org>
2015-11-16 19:16:08 +0200

    Controller added and routes to enable Econt API end-points.

--- src/Http/Controllers/EcontController.php
+++ src/Http/Controllers/EcontController.php
@@ -2,6 +2,10 @@
 namespace Rolice\Econt\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Rolice\Econt\Models\Neighbourhood;
+use Rolice\Econt\Models\Office;
+use Rolice\Econt\Models\Settlement;
+use Rolice\Econt\Models\Street;
 use Rolice\Econt\Models\Zone;
 
 class EcontController extends Controller
@@ -9,5 +13,25 @@ class EcontController extends Controller
     public function zones()
     {
         return Zone::orderBy('name')->get();
+    }
+
+    public function settlements()
+    {
+        return Settlement::with('country')->orderBy('name')->limit(10)->get();
+    }
+
+    public function neighbourhoods()
+    {
+        return Neighbourhood::orderBy('name')->get();
+    }
+
+    public function streets()
+    {
+        return Street::orderBy('name')->get();
+    }
+
+    public function offices()
+    {
+        return Office::orderBy('name')->get();
     }
 }
--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,5 +1,9 @@
 <?php
 
 Route::group(['prefix' => 'econt'], function () {
-    Route::get('zones', ['as' => 'econt.zones', 'EcontController@zones']);
+    Route::get('zones', ['as' => 'econt.zones', 'Rolice\Econt\Http\Controllers\EcontController@zones']);
+    Route::get('settlements', ['as' => 'econt.settlements', 'Rolice\Econt\Http\Controllers\EcontController@settlements']);
+    Route::get('neighbourhoods', ['as' => 'econt.neighbourhoods', 'Rolice\Econt\Http\Controllers\EcontController@neighbourhoods']);
+    Route::get('streets', ['as' => 'econt.streets', 'Rolice\Econt\Http\Controllers\EcontController@streets']);
+    Route::get('offices', ['as' => 'econt.offices', 'Rolice\Econt\Http\Controllers\EcontController@offices']);
 });
