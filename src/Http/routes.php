df3a1fe7de810029e22e419fbe8adf07c2b53dc0
Rolice <l.gardev@intellisys.org>
2015-11-16 19:33:52 +0200

    Fix on routes.

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,9 +1,9 @@
 <?php
 
 Route::group(['prefix' => 'econt'], function () {
-    Route::get('zones', ['as' => 'econt.zones', 'Rolice\Econt\Http\Controllers\EcontController@zones']);
-    Route::get('settlements', ['as' => 'econt.settlements', 'Rolice\Econt\Http\Controllers\EcontController@settlements']);
-    Route::get('neighbourhoods', ['as' => 'econt.neighbourhoods', 'Rolice\Econt\Http\Controllers\EcontController@neighbourhoods']);
-    Route::get('streets', ['as' => 'econt.streets', 'Rolice\Econt\Http\Controllers\EcontController@streets']);
-    Route::get('offices', ['as' => 'econt.offices', 'Rolice\Econt\Http\Controllers\EcontController@offices']);
+    Route::get('zones', ['as' => 'econt.zones', 'uses' => 'Rolice\Econt\Http\Controllers\EcontController@zones']);
+    Route::get('settlements', ['as' => 'econt.settlements', 'uses' => 'Rolice\Econt\Http\Controllers\EcontController@settlements']);
+    Route::get('neighbourhoods', ['as' => 'econt.neighbourhoods', 'uses' => 'Rolice\Econt\Http\Controllers\EcontController@neighbourhoods']);
+    Route::get('streets', ['as' => 'econt.streets', 'uses' => 'Rolice\Econt\Http\Controllers\EcontController@streets']);
+    Route::get('offices', ['as' => 'econt.offices', 'uses' => 'Rolice\Econt\Http\Controllers\EcontController@offices']);
 });
