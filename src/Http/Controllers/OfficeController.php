235f261b0f6b14db89a5f774cd8264eec4bfdd33
Rolice <l.gardev@intellisys.org>
2015-11-18 13:34:54 +0200

    Added office autocomplete.

--- src/Http/Controllers/OfficeController.php
+++ src/Http/Controllers/OfficeController.php
@@ -2,12 +2,7 @@
 namespace Rolice\Econt\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Rolice\Econt\Models\Neighbourhood;
 use Rolice\Econt\Models\Office;
-use Rolice\Econt\Models\Settlement;
-use Rolice\Econt\Models\Street;
-use Rolice\Econt\Models\Zone;
-
 use Rolice\Econt\Helpers\Locale;
 
 class OfficeController extends Controller
