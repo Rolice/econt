6457832e30db481247fe18b0fa6f89896451d904
Rolice <l.gardev@intellisys.org>
2015-11-18 11:00:56 +0200

    Settlement controller added and new routes are defined for them.

--- src/Http/Controllers/SettlementController.php
+++ src/Http/Controllers/SettlementController.php
@@ -1,12 +1,10 @@
 <?php
 namespace Rolice\Econt\Http\Controllers;
 
+use Input;
+
 use App\Http\Controllers\Controller;
-use Rolice\Econt\Models\Neighbourhood;
-use Rolice\Econt\Models\Office;
 use Rolice\Econt\Models\Settlement;
-use Rolice\Econt\Models\Street;
-use Rolice\Econt\Models\Zone;
 
 class SettlementController extends Controller
 {
