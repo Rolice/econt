c6adcf7ba165cbf5569138715dc0e9869bf3a9b6
Rolice <l.gardev@intellisys.org>
2015-12-01 12:30:42 +0200

    Improvement of shipping. Added currency which seems to have no effect on any response.

--- src/Components/Shipment.php
+++ src/Components/Shipment.php
@@ -6,11 +6,12 @@ class Shipment implements ComponentInterface
 
     use Serializable;
 
+    public $weight;
+    public $currency;
     public $envelope_num;
     public $shipment_type;
     public $description;
     public $pack_count;
-    public $weight;
     public $send_date;
     public $tariff_code;
     public $tariff_sub_code;
