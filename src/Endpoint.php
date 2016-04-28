b51ac6a47e285a1d65592da4f4bed1aa73b75414
Rolice <l.gardev@intellisys.org>
2016-04-28 15:12:33 +0300

    Added new ENV variable for econt evnironment, separately from laravel env.

--- src/Endpoint.php
+++ src/Endpoint.php
@@ -20,12 +20,12 @@ class Endpoint
 
     public static function parcel()
     {
-        return 'production' === env('APP_ENV', 'production') ? self::PARCEL : self::PARCEL_DEMO;
+        return 'production' === env('ECONT_ENV', env('APP_ENV', 'production')) ? self::PARCEL : self::PARCEL_DEMO;
     }
 
     public static function service()
     {
-        return 'production' === env('APP_ENV', 'production') ? self::SERVICE : self::SERVICE_DEMO;
+        return 'production' === env('ECONT_ENV', env('APP_ENV', 'production')) ? self::SERVICE : self::SERVICE_DEMO;
     }
 
 }
